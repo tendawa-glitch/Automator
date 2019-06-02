150bccaaad5f778a8421db640adb60ddb6b51a4f
tendawa123 <tnzngdw@gmail.com>
2019-06-02 12:11:01 +0530

    first commit

--- src/main/java/com/example/demo/AutomaterApplication.java
+++ src/main/java/com/example/demo/AutomaterApplication.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class AutomaterApplication {
 
 	private final static String URL = "https://mp.weixin.qq.com/s?__biz=MzI0MTAzMjM3Ng==&mid=2651146626&idx=1&sn=c78f22d3a2c770c9b15c4e7d5e055c97&chksm=f2e02455c597ad43fa96214f20b8c2ed046a51befe55ae6a7910633fe694a4b3fa15b7fc2ac2&mpshare=1&scene=1&srcid=0811CP9DnTGJXGIbZkLOcpbZ#rd";
@@ -19,7 +19,7 @@ public class AutomaterApplication {
 	private final static String fileSourceDownloadUrl = "https://res.wx.qq.com/voice/getvoice?mediaid=";
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\automator\\chromedriver.exe");
 		WebDriver webDriver = new ChromeDriver();
 		webDriver.get(URL);
 		try {
@@ -48,7 +48,7 @@ public class AutomaterApplication {
 			wait.until(ExpectedConditions.elementToBeClickable(By.className("share_audio_title"))).click();
 			System.out.println("voice ID  from the URL : " + webDriver.getCurrentUrl().substring(109, 137));
 			downloadUsingNIO(fileSourceDownloadUrl + webDriver.getCurrentUrl().substring(109, 137),
-					"E:/mp3/audio" + element + increment + ".mp3");
+					"C:/automator/mp3/audio" + element + increment + ".mp3");
 			webDriver.navigate().back();
 		} catch (Exception e) {
 			e.printStackTrace();
